5254f8ecd41c6e9a27678a6c13f1734474768399
Ghazaleh Ansari <ghazaaleh.ansari@gmail.com>
2024-04-26 11:34:56 +0200

    Data is fetched for main page by get http request

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header.jsx";
+import Meals from "./components/Meals.jsx";
 
 function App() {
   return (
     <>
       <Header />
+      <Meals />
     </>
   );
 }
