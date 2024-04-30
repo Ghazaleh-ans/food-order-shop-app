d40e7b9755191bc3e2b6ada8a8809687b53918bb
Ghazaleh Ansari <ghazaaleh.ansari@gmail.com>
2024-04-30 13:17:13 +0200

    Modal is created

---
+++ src/components/UI/Modal.jsx
@@ -0,0 +1,19 @@
+import { createPortal } from "react-dom";
+import { useEffect, useRef } from "react";
+
+export default function Modal({ children, open, className = "" }) {
+  const dialog = useRef();
+
+  useEffect(() => {
+    if (open) {
+      dialog.current.showModal();
+    }
+  }, [open]);
+
+  return createPortal(
+    <dialog ref={dialog} className={`modal ${className}`}>
+      {children}
+    </dialog>,
+    document.getElementById("modal")
+  );
+}
