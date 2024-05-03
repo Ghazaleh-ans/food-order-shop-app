e5a7a0ef62a2b87f35fc4819e85b3d8cc4e1fa60
Ghazaleh Ansari <ghazaaleh.ansari@gmail.com>
2024-05-03 20:06:24 +0200

    Custom HTTP hook is created

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,21 +1,22 @@
-import { useState, useEffect } from "react";
 import MealItem from "./MealItem.jsx";
+import useHttp from "../hooks/useHttp.js";
+
+const requestConfig = {};
 
 export default function Meals() {
-  const [loadedMeals, setLoadedMeals] = useState([]);
+  const {
+    data: loadedMeals,
+    isLoading,
+    error,
+  } = useHttp("http://localhost:3000/meals", requestConfig, []);
 
-  useEffect(() => {
-    async function fetchMeals() {
-      const response = await fetch("http://localhost:3000/meals");
-      if (!response.ok) {
-        //...
-      }
-      const meals = await response.json();
-      setLoadedMeals(meals);
-    }
+  if (isLoading) {
+    return <p>Fetching meals ...</p>;
+  }
 
-    fetchMeals();
-  }, []);
+  // if (!data) {
+  //   return <p>No meals found!</p>;
+  // }
 
   return (
     <ul id="meals">
